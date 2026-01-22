40eab8c0227ea14b8e474adce57e01c9fc03421e
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2026-01-22 10:52:20 +0100

    WIP: creating server.cpp

--- model/include/Server.hpp
+++ model/include/Server.hpp
@@ -1,4 +1,29 @@
 #ifndef WIZZMANIA_SERVER_HPP
 #define WIZZMANIA_SERVER_HPP
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+class Server {
+    public:
+    Server();
+    ~Server();
+
+
+
+
+
+
+    private:
+
+    int sockfd; //socket file descriptor
+    int newsockfd; //new socket file descriptor
+    int portno; // port number
+
+
+};
 #endif //WIZZMANIA_SERVER_HPP
